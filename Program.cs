7231551cd6654da2d79a41108f315d55ae369125
Vitor Guimarães <ovitordeoliveiraguimaraes@gmail.com>
2026-09-09 20:49:52 -0300

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using Spectre.Console;
+
+class Program
+{
+    public static string user = "BVITOR";
+    public static string password = "1234";
+    static void Main(string[] args)
+    {
+        Login(user, password);
+
+        Ui();
+
+        switch (Menu())
+        {
+            case "Sair":
+
+            Console.WriteLine();
+
+            AnsiConsole.Write(
+                Align.Center(
+                    new Panel("[red]Saindo...[/] Até a próxima!")
+                    .BorderColor(Color.Red)
+                    .Border(BoxBorder.Rounded)
+                )
+            );
+
+            Thread.Sleep(2000);
+
+                break;
+        }
+    }
+
+    static void Ui()
+    {
+        Console.Clear();
+
+        AnsiConsole.Write(
+            new Rows(
+                new Rule(),
+                new Rule(),
+                new FigletText("nexus")
+                .Centered()
+                .Color(Color.Blue),
+                new Markup("[bold white]Console App[/]")
+                .Centered()
+            )
+        );
+    }
+
+    static string Menu()
+    {
+        string option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices("Consultar", "Cadastrar", "Sair")
+        );
+
+        return option;
+    }
+
+    static void Login(string user, string password)
+    {
+        Ui();
+
+        Console.WriteLine();
+
+        string readUser = AnsiConsole.Ask<string>(
+            "Usuário: "
+        ).ToUpper();
+
+        string readPass = AnsiConsole.Ask<string>(
+            "Senha: "
+        );
+
+        if(readUser == user && readPass == password)
+        {
+            return;
+        }
+
+        else
+        {
+            AnsiConsole.Write(
+                Align.Center(
+                    new Panel("[bold red]ACESSO NEGADO[/]")
+                    .BorderColor(Color.Red)
+                    .Border(BoxBorder.Rounded)
+                )
+            );
+
+            Console.CursorVisible = false;
+            Thread.Sleep(3000);
+            Console.CursorVisible = true;
+
+            Login(user, password);
+        }
+    }
+}
